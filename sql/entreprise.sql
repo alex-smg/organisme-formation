614d90218c8a1dd9f529113638e0285c7e23120e
Vincent Schlosser <s.vincent@live.fr>
2019-03-24 11:20:43 +0100

    add data

---
+++ sql/entreprise.sql
@@ -0,0 +1,5 @@
+insert into entreprise (identreprise, nom) values (1, 'Credit Acceptance Corporation');
+insert into entreprise (identreprise, nom) values (2, 'Axis Capital Holdings Limited');
+insert into entreprise (identreprise, nom) values (3, 'Ennis, Inc.');
+insert into entreprise (identreprise, nom) values (4, 'Teradata Corporation');
+insert into entreprise (identreprise, nom) values (5, 'Cypress Energy Partners, L.P.');
