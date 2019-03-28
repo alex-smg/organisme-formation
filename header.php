fab9f1dc59a993826abdbf587ef431e5f0c8aedd
alexandresmagghe <alexandre-smagghe@outlook.com>
2019-03-28 13:21:48 +0100

    add show eleve

--- header.php
+++ header.php
@@ -4,13 +4,12 @@ require_once 'bootstrap.php';
 <html>
     <head>
         <meta charset="UTF-8">
-        <link rel='stylsheet' href='../css/style.css'>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <!-- <link rel='stylesheet' href="../css/bootstrap-lux.css"> -->
+        <link rel='stylesheet' href='../css/style.css'>
     </head>
 <header>
     <h1>OFS</h1>
-    <h2>Organisme de formation des salariès</h2>
     
     <ul>
         <li>
--- templates/show_eleve.php
+++ templates/show_eleve.php
@@ -5,17 +5,90 @@ include '../header.php';
 
 
 
-    if ($_GET['id']) {
+    if (! empty($_GET['id'])) {
         $Eleve = $entityManager->getRepository("Eleve")->findBy(array('ideleve' => $_GET));
-         foreach ($Eleve as $key => $entreprise) {
-        $result =  $entreprise->getEntreprise();
+
+        foreach ($Eleve as $key => $entreprise) {
+        $resultEntreprise =  $entreprise->getEntreprise();
             }
 
-        $Entreprise = $entityManager->getRepository("Entreprise")->findBy(array('identreprise' => $result));
-        $findEleve = $_GET['id'];
+        $Entreprise = $entityManager->getRepository("Entreprise")->findBy(array('identreprise' => $resultEntreprise));
 
-        echo $findEleve;
 
+        $Participation = $entityManager->getRepository("Participation")->findBy(array('eleve' => $_GET));
+
+        foreach ($Participation as $key => $participation) {
+            $resultSession =  $participation->getSession();
+        }
+
+        $sessionEleve = $entityManager->getRepository("Session")->findBy(array('idsession' => $resultSession));
+
+        foreach ($sessionEleve as $key => $session) {
+            $formation = $session->getFormation();
+        }
+
+        $formationEleve = $entityManager->getRepository('Formation')->findBy(array('idformation' => $formation));
+
+
+        $allSession = $entityManager->getRepository('Session')->findAll();
+
+
+
+        ?><section class="eleve">
+    <h3>Nom de l'eleve :</h3>
+    <ul><?php foreach ($Eleve as $key => $eleve) {
+            echo $eleve->getNom();
+        }
+        ?></ul>
+    <h3> Entreprise:</h3>
+    <ul><?php foreach ($Entreprise as $key => $entreprise) {
+            echo $entreprise->getNom();
+        }
+        ?></ul>
+    <h3> Formation suivi:</h3>
+    <ul>
+        <?php foreach ($formationEleve as $key => $formation) {
+            echo '<li>'. $formation->getNom() . '</li>';
+        }
+        ?>
+    </ul>
+</section>
+
+<section>
+    <h2>S'inscrire à une session</h2>
+
+    <form method="post">
+
+
+        <div class="form-group">
+            <label for="session">Session</label>
+            <select name="session" id="session">
+                <?php
+                    foreach ($allSession as $key => $session) {
+                        echo "<option value='" . $session->getIdsession() . "'>"  . $session->getDateDebut()->format('d-m-Y H') . " - " . $session->getDateFin()->format('d-m-Y H') . "</option>";
+                    }
+                ?>
+            </select>
+        </div>
+
+        <input type="submit" class="btn btn-primary" value="Enregistrer" name="envoyer">
+    </form>
+</section>
+
+
+<script>
+    var select1 = document.querySelector('#session')
+
+    select1.addEventListener('change')
+
+</script>
+
+<?php
+
+}else{
+
+        echo'<h3>Vous devez choisir un eleve ! </h3>';
+        echo '<a href="http://localhost:8888/organisme-formation/templates/show_eleves_list.php">Liste des eleves</a>';
     }
 
 
@@ -27,57 +100,9 @@ include '../header.php';
         $entityManager->flush();
     }
 
-    $allFormation = $entityManager->getRepository('Formation')->findAll();
-    $allSession = $entityManager->getRepository('Session')->findAll();
+
+
 ?>
 
 
 
-<section>
-    <h1>Nom de l'eleve :</h1>
-    <ul><?php foreach ($Eleve as $key => $eleve) {
-        echo $eleve->getNom();
-            }
-?></ul>
-    <h1> Entreprise:</h1>
-    <ul><?php foreach ($Entreprise as $key => $entreprise) {
-        echo $entreprise->getNom();
-        }
-        ?></ul>
-    <ul>
-<?php
-/*
-*/?>
-    </ul>
-</section>
-
-    <section>
-        <h2>S'inscrire à une session</h2>
-
-        <form method="post">
-
-            <div class="form-group">
-                <label for="formation">Formation</label>
-                <select name="formation" id="formation">
-                    <?php
-                        foreach ($allFormation as $key => $formation) {
-                            echo "<option value='" . $formation->getIdformation() . "'>"  . $formation->getNom() . "</option>";
-                        }
-                    ?>
-                </select>
-            </div>
-
-            <div class="form-group">
-                <label for="session">Session</label>
-                <select name="session" id="session">
-                    <?php
-                        foreach ($allSession as $key => $session) {
-                            echo "<option value='" . $session->getIdsession() . "'>"  . $session->getDateDebut()->format('d-m-Y H') . " - " . $session->getDateFin()->format('d-m-Y H') . "</option>";
-                        }
-                    ?>
-                </select>
-            </div>
-
-            <input type="submit" class="btn btn-primary" value="Enregistrer" name="envoyer">
-        </form>
-    </section>
